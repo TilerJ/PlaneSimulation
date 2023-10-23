54d486bdab2566659702d6fef8c4f9ad8641e33a
David <147713888+s2236581@users.noreply.github.com>
2023-10-23 12:27:29 +0100

    Update aero_analytical_build_ToBeCompleted.py

--- aero_analytical_build_ToBeCompleted.py
+++ aero_analytical_build_ToBeCompleted.py
@@ -1,2 +1,2 @@
-wb = -sim.L/m * cos(sim.alpha) - sim.D/m * sin(sim.alpha) + sim.q*ub + W/m
+dwb = -sim.L/m * np.cos(sim.alpha) - sim.D/m * np.sin(sim.alpha) + sim.q*ub + W/m
 wb = sim.Velocity * sin(sim.alpha)
